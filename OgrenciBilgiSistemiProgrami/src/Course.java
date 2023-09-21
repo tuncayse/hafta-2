b97011a6967502314283149aad29241d1898a27d
tuncayse <143759353+tuncayse@users.noreply.github.com>
2023-09-21 18:43:42 +0300

    Ogrenci Bilgi Sistemi Programi

---
+++ OgrenciBilgiSistemiProgrami/src/Course.java
@@ -0,0 +1,31 @@
+public class Course {
+    Teacher teacher;
+    String name;
+    String code;
+    String prefix;
+    int note;
+    Course(String name, String code, String prefix){
+        this.name = name;
+        this.code = code;
+        this.prefix = prefix;
+        int note = 0;
+    }
+    void addTeacher(Teacher teacher){
+        this.teacher = teacher;
+        if (teacher.branch.equals(this.prefix)){
+            System.out.println("Islem Basarili");
+        }
+        else{
+            System.out.println("Ogretmen ve ders uyusmuyor.");
+        }
+    }
+    void printTeacherInfo(){
+
+        if (teacher != null) {
+            System.out.println(this.name + " dersinin Akademisyeni : " + teacher.name);
+        } else {
+            System.out.println(this.name + " dersine Akademisyen atanmamıştır.");
+        }
+    }
+
+}
